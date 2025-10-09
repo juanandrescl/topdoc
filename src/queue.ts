76674b0a846d4700240da0a7043ac84598ddf2d6
juanandrescl <obvioquesi@gmail.com>
2025-10-09 10:48:51 +0200

    ok

--- src/queue.ts
+++ src/queue.ts
@@ -5,6 +5,7 @@ export const labResultsQueue = new Queue('lab-results', {
   connection: REDIS_CONFIG
 });
 
+
 export const deadLetterQueue = new Queue('dead-letter', {
   connection: REDIS_CONFIG
 });
